9e840bbe90f4cdbfd7204471d4ab803ff972c5a9
AsboStew08 <70031387+AsboStew08@users.noreply.github.com>
2021-02-01 17:11:38 -0500

    Add files via upload

---
+++ ReadStoryScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Stylesheet, Text, View} from 'react-native';
+
+export default class ReadStoryScreen extends React.Component{
+    render(){
+        return(
+            <View style={Stylesheet.container}>
+                <Text>Read Story</Text>
+            </View>
+        );
+    }
+} 
+
+const styles = StyleSheet.create({
+    containter: {
+        
+    }
+})
